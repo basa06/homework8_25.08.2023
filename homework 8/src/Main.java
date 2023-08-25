a7e1a8995d6b8212f1537261530ad210eb57cbe7
basa06 <basa.akshaya@syk.fi>
2023-08-25 11:45:17 +0300

    Initial commit

---
+++ homework 8/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       int a=4, i=0, b=3, result=1;
+
+       while (i<a){
+           result=result*b;
+           i++;
+
+       }
+        System.out.println(result);
+       //answer for code that calculates b^a, for 3^a just substitute b for 3 in the while loop.
+    }
+}
